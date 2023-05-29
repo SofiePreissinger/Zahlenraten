ff18dbe5174d06a0f443425d765ec9ffa6398321
SofiePreissinger <132834005+SofiePreissinger@users.noreply.github.com>
2023-05-29 18:45:47 +0200

    Add files via upload

---
+++ Zahlenraten.py
@@ -0,0 +1,41 @@
+import random
+def Ratespiel():
+    secretNumber = random.randint(1, 20)
+    print('Erraten Sie eine Zahl zwischen 1 und 20 mit maximal 5 Versuchen')
+
+    for guessesTaken in range (1,6):
+        print('Erraten Sie die Zahl')
+        guess = int(input())
+
+        if guess < secretNumber:
+            print(f'Die Zahl is höher als {guess} !')
+        elif guess > secretNumber:
+         print(f'Die Zahl ist kleiner als {guess} !')
+        else:
+            break
+
+    if guess == secretNumber:
+        if str(guessesTaken) : 1
+        print('Gut gemacht! Sie haben ' + str(guessesTaken) + ' mal geraten')
+    else:
+        print('Die zufällige Nummer war ' + str(secretNumber))
+
+    nochmal()
+
+def nochmal():
+
+    nochmalspielen = input('''
+Wollen Sie es nochmal versuchen ?
+Bitte geben Sie bitte "Ja" oder "Nein" ein.
+''')
+
+    if nochmalspielen == 'Ja':
+        Ratespiel()
+
+    elif nochmalspielen == 'Nein':
+        print ('Spiel beendet.')
+
+    else:
+        nochmal()
+
+Ratespiel()
